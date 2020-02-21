2dc66e1097d0a943232d7bf37a9401ac61abae97
dani0609 <danishriaz0609@outlook.com>
2020-02-21 22:52:44 +0500

    js-comparison_opeartor

---
+++ FRONT-END/JS/comparison_operator.js
@@ -0,0 +1,10 @@
+var name = prompt("Enter your name.");
+var age = prompt("Enter your age.");
+if(name == "john")
+{
+  alert("Welcome John!");
+}
+if(age >= 18)
+{
+  alert("Cong ! you are eligible");
+}
